34486b9d04a2423d07437a2341bca756a30093e3
umekishimaru <riki12344649@gmail.com>
2024-11-03 22:02:02 +0900

    画像表示

---
+++ thanksday/src/component/Module.jsx
@@ -0,0 +1,44 @@
+export const members = [
+  { name: 'まつかわX仙台のフルリモートエンジニア', url: "ファイル19.jpg" },
+  { name: 'padoさん', url: "ファイル1.jpg" },
+  { name: 'yukiさん', url: "ファイル6.jpg" },
+  { name: 'tohruさん', url: "ファイル17.jpg" },
+  { name: 'NabEさん', url: "ファイル16.jpg" },
+  { name: 'Ryutaさん', url: "ファイル15.jpg" },
+  { name: 'Kuro@エンジニア3年目さん', url: "ファイル14.jpg" },
+  { name: 'Taira Nobuhikoさん', url: "ファイル13.jpg" },
+  { name: '小南美佳さん', url: "ファイル12.jpg" },
+  { name: 'しっしーさん', url: "ファイル20.jpg" },
+  { name: 'Sayuさん', url: "ファイル11.jpg" },
+  { name: 'tak220さん', url: "ファイル10.jpg" },
+  { name: 'isnowballさん', url: "ファイル8.jpg" },
+  { name: 'グンコアさん(とりむぎさん)', url: "ファイル7.jpg" },
+  { name: 'まみさん', url: "ファイル5.jpg" },
+  { name: 'manaさん', url: "ファイル3.jpg" },
+  { name: '佐々木りささん', url: "ファイル4.jpg" },
+  { name: 'ゆうきさん', url: "ファイル2.jpg" },
+  { name: 'Keiさん', url: "ファイル18.jpg" }
+];
+
+
+// const members = [
+//   'まつかわX仙台のフルリモートエンジニア',
+//   'padoさん',
+//   'yukiさん',
+//   'tohruさん',
+//   'NabEさん',
+//   'Ryutaさん',
+//   'Kuro@エンジニア3年目さん',
+//   'Taira Nobuhikoさん',
+//   '小南美佳さん',
+//   'しっしーさん',
+//   'Sayuさん',
+//   'tak220さん',
+//   'isnowballさん',
+//   'グンコアさん(とりむぎさん)',
+//   'まみさん',
+//   'manaさん',
+//   '佐々木りささん',
+//   'ゆうきさん',
+//   'Keiさん'
+// ];
--- thanksday/src/component/Name.jsx
+++ thanksday/src/component/Name.jsx
@@ -4,20 +4,16 @@ import { MyCompornent } from './Winner'
 const Name = ({ members ,stopShuffle}) => {
   const [winner, changeWinner] = useContext(MyCompornent)
 
-  // stopを押された後にスタート押したらwinnerをまた更新する。そうしないと２回目以降のスタートが機能しない
-
-
-
   useEffect(() => {
     if (stopShuffle) return;
     const randomIndex = Math.floor(Math.random() * members.length)
-    changeWinner(winner => ({ ...winner, name: members[randomIndex].name }))
+    changeWinner(winner => ({name: members[randomIndex].name,url: members[randomIndex].url }))
   }, [winner])
 
   return (
     <div>
       <p>{winner.name}</p>
-      {/* <image></image> */}
+      <img src={`/img/${winner.url}`} width="50px" height="50px"alt="サンプル画像" align="top" ></img>
     </div>
   )
 }
